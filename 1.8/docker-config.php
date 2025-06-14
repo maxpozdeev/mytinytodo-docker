f4d8eda2cd38c117033d51e343da80a84e7331c0
maxpozdeev <maxpozdeev@gmail.com>
2025-06-14 19:10:50 +0300

    v1.8.3

--- 1.8/docker-config.php
+++ 1.8/docker-config.php
@@ -2,8 +2,10 @@
 
 // Rename it to config.php before using in docker.
 
-if (getenv('MTT_DB_TYPE') == 'mysql' || getenv('MTT_DB_TYPE') == 'postgres') {
-    define("MTT_DB_TYPE", getenv('MTT_DB_TYPE'));
+define("_MTT_DB_TYPE", getenv('MTT_DB_TYPE') ?: "");
+
+if (_MTT_DB_TYPE == 'mysql' || _MTT_DB_TYPE == 'postgres') {
+    define("MTT_DB_TYPE", _MTT_DB_TYPE);
     define("MTT_DB_HOST", getenv('MTT_DB_HOST') ?: "undefined_host");
     define("MTT_DB_NAME", getenv('MTT_DB_NAME') ?: "undefined_db");
     define("MTT_DB_USER", getenv('MTT_DB_USER') ?: "undefined_user");
@@ -11,8 +13,8 @@ if (getenv('MTT_DB_TYPE') == 'mysql' || getenv('MTT_DB_TYPE') == 'postgres') {
     define("MTT_DB_PREFIX", getenv('MTT_DB_PREFIX') ?: "mtt_");
     define("MTT_DB_DRIVER", getenv('MTT_DB_DRIVER') ?: "");
 }
-else if (getenv('MTT_DB_TYPE') == 'sqlite') {
-    define("MTT_DB_TYPE", "sqlite");
+else if (_MTT_DB_TYPE == 'sqlite') {
+    define("MTT_DB_TYPE", _MTT_DB_TYPE);
     define("MTT_DB_PREFIX", "");
 }
 
